9c162838e572f1257fc49db9721c86b355b6cd52
AlyszaZealand <als012@edu.zealand.dk>
2026-03-20 12:17:19 +0100

    commit

--- src/test/java/com/example/strategypattern/Validate/SimpleValidationStrategyTest.java
+++ src/test/java/com/example/strategypattern/Validate/SimpleValidationStrategyTest.java
@@ -1,8 +1,5 @@
 package com.example.strategypattern.Validate;
 
-import com.example.strategypattern.Service.UserService;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
--- src/test/java/com/example/strategypattern/Validate/StrictValidationStrategyTest.java
+++ src/test/java/com/example/strategypattern/Validate/StrictValidationStrategyTest.java
@@ -1,7 +1,5 @@
 package com.example.strategypattern.Validate;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
